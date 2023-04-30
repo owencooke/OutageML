68108aefce2b3e28858b8cc923f2b1c6193a4cb4
Jared Drueco <drueco03@gmail.com>
2023-04-30 06:14:58 -0600

    I moved a div by 1 px cus it was bothering me

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="bg-white py-6 px-8 shadow-inner border-b-[1px] border-black h-20">
       <div className="mx-auto flex justify-between">
         <div className="font-bold text-xl flex items-start">
-          <IoPower className="text-3xl" />
+          <IoPower className="text-3xl pb-[2px]" />
           utageML
         </div>
         <div className="text-xl text-gray-500">Sign out</div>
